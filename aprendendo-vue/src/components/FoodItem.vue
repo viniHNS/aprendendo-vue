d22add7af522d01186b092e361f374db6a482748
Vinicius Hissayoshi Nishida <88986579+viniHNS@users.noreply.github.com>
2023-10-25 14:24:56 +0000

    commit rushado

---
+++ aprendendo-vue/src/components/FoodItem.vue
@@ -0,0 +1,31 @@
+<template>
+    <div v-on:click="click">
+        <h2>{{ name }}</h2>
+        <p>{{ description }}</p>
+        <p id="red">Você clicou aqui {{ clicks }} vezes</p>
+    </div>
+</template>
+
+<script>
+    export default{
+        data(){
+            return{
+                name: 'Maçã',
+                description: 'Uma fruta muito gostosa',
+                clicks: 0
+            }
+        },
+        methods: {
+            click(){
+                this.clicks++
+            }
+        },
+    }
+</script>
+
+<style>
+    #red {
+    font-weight: bold ;
+    color: rgb(144, 12, 12);
+}
+</style>
